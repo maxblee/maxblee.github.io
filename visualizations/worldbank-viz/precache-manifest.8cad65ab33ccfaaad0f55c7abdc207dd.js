3a8b92646884a27c6e6ad3570c62c3451418abb8
Max Lee <maxbmhlee@gmail.com>
2020-10-05 15:30:32 -0400

    added world bank visualization

---
+++ visualizations/worldbank-viz/precache-manifest.8cad65ab33ccfaaad0f55c7abdc207dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51255d5bbad3dbd5b653cccce157c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b279d38155361389f87f",
+    "url": "/static/css/main.5e369f6d.chunk.css"
+  },
+  {
+    "revision": "a2a81855122868f658e2",
+    "url": "/static/js/2.56e4aa92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56e4aa92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b279d38155361389f87f",
+    "url": "/static/js/main.c601ce98.chunk.js"
+  },
+  {
+    "revision": "5fdc42ea9234e05c51f6",
+    "url": "/static/js/runtime-main.a9757b80.js"
+  }
+]);
